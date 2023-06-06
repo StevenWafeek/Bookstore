d7cc052cd5c38497a05b10ca579bce51bd8b553a
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-07 00:54:38 +0300

    Add books to component

---
+++ src/components/books.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Book(props) {
+  const { title, author, onDelete } = props;
+
+  const handleDelete = () => {
+    onDelete();
+  };
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>
+        Author:
+        {author}
+      </p>
+      <button type="button" onClick={handleDelete}>Delete</button>
+    </div>
+  );
+}
+
+export default Book;
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
